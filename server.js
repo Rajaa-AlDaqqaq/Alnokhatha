1b2ec39864f5259ce3ed66875730b7b7d6e7ef53
Zahra Alaradi <zahrabdulla17@hotmail.com>
2023-10-18 17:49:24 +0300

    Sever.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
+//Morgon logs //npm install morgan when use it
+
+const PORT = process.env.PORT || 3001
+
+const db = require('./db')
+
+const app = express()
+
+app.use(cors())
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', (req, res) => {
+  res.send(`Connected!`)
+})
+
+app.listen(PORT, () => {
+  console.log(`Running Express server on Port ${PORT} . . .`)
+})
