8e1fd8e1e2f9860b6e77ba75569f4dbccb9639f8
Zainab <zainabsameer197@gmail.com>
2023-10-20 13:04:09 +0300

    users

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
-//Morgon logs //npm install morgan when use it
 
 const PORT = process.env.PORT || 3001
+
+const AuthRouter = require('./routes/AuthRouter')
 
 const db = require('./db')
 
@@ -13,6 +14,8 @@ app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+app.use('/auth', AuthRouter)
 
 app.use('/', (req, res) => {
   res.send(`Connected!`)
