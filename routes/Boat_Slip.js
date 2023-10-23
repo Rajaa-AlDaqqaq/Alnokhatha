606bb1862e5abd112a3b6b8786dbfbb201558dca
Zahra Alaradi <zahrabdulla17@hotmail.com>
2023-10-23 13:14:01 +0300

    LAST CHANGE

--- routes/Boat_Slip.js
+++ routes/Boat_Slip.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const slipCtrl = require('../controllers/Boat_Slip')
+router.get('/slip/:boatSlip_id', slipCtrl.GetSlip)
 router.post('/slips', slipCtrl.GetSlips)
 router.put('/update/:boatSlip_id', slipCtrl.UpdateSlip)
 router.post('/create', slipCtrl.slip_create_post)
--- routes/Harbor.js
+++ routes/Harbor.js
@@ -1,7 +1,21 @@
+const multer = require('multer')
 const router = require('express').Router()
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '_' + Date.now() + '_' + file.originalname)
+  }
+})
+var upload = multer({
+  storage: storage
+}).single('path')
+
 const harborCtrl = require('../controllers/Harbor')
 router.get('/harbors', harborCtrl.GetHarbors)
 router.post('/create', harborCtrl.CreateHarbor)
 router.put('/update/:harbor_id', harborCtrl.UpdateHarbor)
+router.get('/:harbor_id', harborCtrl.GetHarbor)
 router.delete('/delete/:harbor_id', harborCtrl.DeleteHarbor)
 module.exports = router
